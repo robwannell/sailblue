662d04f607f0a0fda6506e894b1eee9f0e8ea530
robwannell <rob@meltemiweb.com>
2015-03-10 11:53:17 -0700

    Init

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+set :application, 'sailblue'
+set :repo_url, 'git@github.com:robwannell/sailblue.git'
+
+set :deploy_to, '/home/deploy/sailblue'
+
+set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
